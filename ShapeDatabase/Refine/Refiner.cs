f41dcf92002ae026d93d1cb571a3b15779800932
GuusDeJonge <g.jonge@students.uu.nl>
2019-09-24 17:53:11 +0200

    Added comments to Refiner

--- ShapeDatabase/Refine/Refiner.cs
+++ ShapeDatabase/Refine/Refiner.cs
@@ -11,21 +11,42 @@ namespace ShapeDatabase.Refine
 {
 	public class Refiner
 	{
+		/// <summary>
+		/// Extends an off file by increasing the number of faces using the Doo-Sabin algorithm.
+		/// </summary>
+		/// <param name="inputFileDirectory">Path from Java file to the input off file</param>
+		/// <param name="outputFileDirectory">Path from Java file to the output off file</param>
 		public static void ExtendMesh(string inputFileDirectory, string outputFileDirectory)
 		{
 			CallJavaScript("doosabin", inputFileDirectory, outputFileDirectory);
 		}
 
+		/// <summary>
+		/// Simplifies an off file by decreasing the number of faces.
+		/// </summary>
+		/// <param name="inputFileDirectory">Path from Java file to the input off file</param>
+		/// <param name="outputFileDirectory">Path from Java file to the output off file</param>
 		public static void SimplifyMesh(string inputFileDirectory, string outputFileDirectory)
 		{
 			CallJavaScript("cleanoff", inputFileDirectory, outputFileDirectory);
 		}
 
+		/// <summary>
+		/// Turns all faces of an off file into triangles.
+		/// </summary>
+		/// <param name="inputFileDirectory">Path from Java file to the input off file</param>
+		/// <param name="outputFileDirectory">Path from Java file to the output off file</param>
 		public static void MakeTriangles(string inputFileDirectory, string outputFileDirectory)
 		{
 			CallJavaScript("tess", inputFileDirectory, outputFileDirectory);
 		}
 
+		/// <summary>
+		/// Calls a Java script located in the Settings.JavaDir directory.
+		/// </summary>
+		/// <param name="inputFileDirectory">Name of the Java script located in the Settings.JavaDir directory</param>
+		/// <param name="inputFileDirectory">Path from Java file to the input off file</param>
+		/// <param name="outputFileDirectory">Path from Java file to the output off file</param>
 		private static void CallJavaScript(string script, string inputFileDirectory, string outputFileDirectory)
 		{
 			string javaPath = Settings.JavaDir;
