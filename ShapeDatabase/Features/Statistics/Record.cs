e2c4727a72b1d20c51ef813e56481343ea3e0a06
Personal-Desktop <kevin.westerbaan@gmail.com>
2019-10-01 22:45:36 +0200

    Records implementation
    Easy way of making snapshots and take records of values.

---
+++ ShapeDatabase/Features/Statistics/Record.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using ShapeDatabase.Util;
+
+namespace ShapeDatabase.Features.Statistics {
+
+	/// <summary>
+	/// A class containing all the different statistics/measures for a single shape.
+	/// </summary>
+	public class Record : IEquatable<Record> {
+
+		#region --- Properties ---
+
+		public DateTime Time { get; }
+		public string Name { get; }
+
+		public ICollection<(string, object)> Measures { get; } =
+			new List<(string, object)>();
+
+		#endregion
+
+		#region --- Constructor Methods ---
+
+		public Record(DateTime time, string name) {
+			Time = time;
+			Name = name ?? throw new ArgumentNullException(nameof(name));
+		}
+
+		#endregion
+
+		#region --- Methods ---
+
+		#region -- Instance Methods --
+
+		public Record AddMeasure(string name, object value) {
+			if (name == null)
+				throw new ArgumentNullException(nameof(name));
+			Measures.Add((name, value));
+			return this;
+		}
+
+		public bool TryGetValue<T>(string name, out T value) {
+			value = default;
+			if (TryGetValue(name, out object result)) {
+				if (result is T) {
+					value = (T) result;
+					return true;
+				} else {
+					throw new InvalidCastException();
+				}
+			}
+			return false;
+		}
+
+		public bool TryGetValue(string name, out object value) {
+			value = default;
+			foreach ((string title, object measure) in Measures)
+				if (name.Equals(title, StringComparison.OrdinalIgnoreCase)) {
+					value = measure;
+					return true;
+				}
+			return false;
+		}
+
+		public object this[string name] {
+			get {
+				if (TryGetValue(name, out object result))
+					return result;
+				throw new KeyNotFoundException();
+			}
+		}
+
+		#endregion
+
+		#region -- Object Methods --
+
+		public override bool Equals(object obj) {
+			return obj is Record && Equals((Record) obj);
+		}
+
+		public bool Equals(Record other) {
+			return other != null
+				&& Time == other.Time
+				&& Name.Equals(other.Name, StringComparison.OrdinalIgnoreCase);
+		}
+
+		public override int GetHashCode() {
+			return NumberUtil.Hash(Time, Name);
+		}
+
+		#endregion
+
+		#endregion
+
+	}
+
+}
---
+++ ShapeDatabase/Features/Statistics/RecordHolder.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using ShapeDatabase.Shapes;
+
+namespace ShapeDatabase.Features.Statistics {
+	
+	/// <summary>
+	/// A class containing a collection of <see cref="Record"/>s
+	/// which is able to create snapshots of databases with its specific measures.
+	/// </summary>
+	public class RecordHolder {
+
+		#region --- Properties ---
+
+		#region -- Exception Messages --
+
+		private const string EX_DUBBLE_SNAP = "The current record already contains a snapshot. " +
+			"Please reset the RecordHolder first before taking another snapshot.";
+
+		#endregion
+
+		#region -- Instance Properties --
+
+		private readonly IDictionary<string, Func<MeshEntry, object>> measures =
+			new Dictionary<string, Func<MeshEntry, object>>();
+
+
+		public bool IsEmpty => SnapshotTime == DateTime.MinValue;
+
+		public bool IsActive { get; private set; } = false;
+
+
+		public DateTime SnapshotTime { get; private set; } = DateTime.MinValue;
+
+		public ICollection<Record> Records { get; private set; } = new List<Record>();
+
+		public IEnumerable<(string, Func<MeshEntry, object>)> Measures {
+			get {
+				foreach(KeyValuePair<string, Func<MeshEntry, object>> pair in measures)
+					yield return (pair.Key, pair.Value);
+			}
+		}
+
+		public IEnumerable<string> MeasureNames {
+			get {
+				// Using the KeyValuePairs instead of the Name Set to guarantee ordering.
+				foreach (KeyValuePair<string, Func<MeshEntry, object>> pair in measures)
+					yield return pair.Key;
+			}
+		}
+
+		#endregion
+
+		#endregion
+
+		#region --- Constructor Methods ---
+
+		public RecordHolder() { }
+
+		public RecordHolder(params (string, Func<MeshEntry, object>)[] measures) {
+			AddMeasure(measures);
+		}
+
+		#endregion
+
+		#region --- Methods ---
+
+		public RecordHolder AddMeasure(string measureName, Func<MeshEntry, object> provider,
+										bool overwrite = false) {
+			if (string.IsNullOrEmpty(measureName))
+				throw new ArgumentNullException(nameof(measureName));
+			if (provider == null)
+				throw new ArgumentNullException(nameof(provider));
+
+			if (overwrite || !measures.ContainsKey(measureName))
+				measures.Add(measureName, provider);
+			
+			return this;
+		}
+
+		public RecordHolder AddMeasure((string, Func<MeshEntry, object>) measure,
+										bool overwrite = false) {
+			return AddMeasure(measure.Item1, measure.Item2, overwrite);
+		}
+
+		public RecordHolder AddMeasure(params (string, Func<MeshEntry, object>)[] measures) {
+			return AddMeasure(false, measures);
+		}
+
+		public RecordHolder AddMeasure(bool overwrite,
+									   params (string, Func<MeshEntry, object>)[] measures) {
+			RecordHolder holder = this;
+
+			if (measures != null)
+				foreach((string name, Func<MeshEntry, object> func) in measures)
+					holder = holder.AddMeasure(name, func, overwrite);
+
+			return holder;
+		}
+
+
+
+		public RecordHolder Reset() {
+			SnapshotTime = DateTime.MinValue;
+			Records = new List<Record>();
+			return this;
+		}
+
+		public RecordHolder TakeSnapShot(MeshLibrary library) {
+			if (!IsEmpty || IsActive)
+				throw new InvalidOperationException(EX_DUBBLE_SNAP);
+			if (library == null)
+				throw new ArgumentNullException(nameof(library));
+
+			IsActive = true;
+			SnapshotTime = DateTime.Now;
+			IList<Record> localRecords = new List<Record>();
+			foreach (MeshEntry entry in library)
+				localRecords.Add(EntrySnapShot(entry));
+
+			Records = localRecords;
+			IsActive = false;
+			return this;
+		}
+
+		private Record EntrySnapShot(MeshEntry entry) {
+			Record record = new Record(SnapshotTime, entry.Name);
+			foreach((string name, Func<MeshEntry, object> provider) in Measures)
+				record.AddMeasure(name, provider(entry));
+			return record;
+		}
+
+		#endregion
+
+	}
+}
