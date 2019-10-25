801d10d3925344f07eac40e566de970a7ad38028
GuusDeJonge <g.jonge@students.uu.nl>
2019-10-25 13:51:18 +0200

    Fix MinMax Normalisation Bug

--- ShapeDatabase/Features/FeatureNormaliser.cs
+++ ShapeDatabase/Features/FeatureNormaliser.cs
@@ -54,7 +54,7 @@ namespace ShapeDatabase.Features
 						double elemValue = elemDesc.Value;
 						double range = MinMaxValues[desc.Name].Item2 - MinMaxValues[desc.Name].Item1;
 
-						double value = range == 0 ? elemValue : (elemValue - MinMaxValues[desc.Name].Item1) / range;
+						double value = (range == 0) ? elemValue : (elemValue - MinMaxValues[desc.Name].Item1) / range;
 
 						normalisedDescriptors[descCount] = new ElemDescriptor(
 							elemName,
@@ -65,7 +65,7 @@ namespace ShapeDatabase.Features
 					} else if (desc is HistDescriptor histDesc)
 						normalisedDescriptors[descCount] = histDesc;
 
-					// Normalisation for unsupported Descriptors.
+					//Normalisation for unsupported Descriptors.
 					else
 						throw new NotImplementedException();
 
@@ -99,80 +99,13 @@ namespace ShapeDatabase.Features
 			foreach (KeyValuePair<string, FeatureVector> vector in vectors)
 				foreach (ElemDescriptor desc in vector.Value.GetDescriptors<ElemDescriptor>())
 				{
-					double Min = desc.Value < MinMaxValues[desc.Name].Item1 ? desc.Value : MinMaxValues[desc.Name].Item1;
-					double Max = desc.Value > MinMaxValues[desc.Name].Item1 ? desc.Value : MinMaxValues[desc.Name].Item2;
+					double Min = (desc.Value < MinMaxValues[desc.Name].Item1) ? desc.Value : MinMaxValues[desc.Name].Item1;
+					double Max = (desc.Value > MinMaxValues[desc.Name].Item2) ? desc.Value : MinMaxValues[desc.Name].Item2;
 					MinMaxValues[desc.Name] = (Min, Max); 
 				}
 
 			// Return the found min and max values per descriptor.
 			return MinMaxValues;
-		}
-
-		/// <summary>
-		/// Calculates the averages of all elementary descriptors.
-		/// </summary>
-		/// <param name="vectors">The featurevectors</param>
-		/// <returns>IDictionary containing all (and only) elementary descriptor names, including their averages.</returns>
-		private static IDictionary<string, double> GetAverages(
-												IDictionary<string, FeatureVector> vectors)
-		{
-			if (vectors == null)
-				throw new ArgumentNullException(nameof(vectors));
-			if (vectors.Count == 0)
-				return new Dictionary<string, double>();
-
-			IDictionary<string, double> averages = new Dictionary<string, double>();
-			FeatureVector exampleVector = vectors.First().Value;
-
-			// Initialise the values for all the elementary descriptors.
-			foreach (ElemDescriptor desc in exampleVector.GetDescriptors<ElemDescriptor>())
-				averages[desc.Name] = 0;
-
-			// Increment the value for all elementary descriptors.
-			foreach (KeyValuePair<string, FeatureVector> vector in vectors)
-				foreach (ElemDescriptor desc in vector.Value.GetDescriptors<ElemDescriptor>())
-					averages[desc.Name] += desc.Value;
-
-			// Normalise all the values in averages.
-			double inverseCount = 1d / vectors.Count;
-			foreach(string name in averages.Keys.ToArray())
-				averages[name] *= inverseCount;
-
-			return averages;
-		}
-
-		/// <summary>
-		/// Calculates the standard deviation of all elementary descriptors.
-		/// </summary>
-		/// <param name="vectors">The featurevectors</param>
-		/// <param name="averages">IDictionary containing all elementary descriptor names, and their averages</param>
-		/// <returns>IDictionary containing all (and only) elementary descriptor names, including their averages.</returns>
-		public static IDictionary<string, double> GetStandardDeviations(
-										IDictionary<string, FeatureVector> vectors,
-										IDictionary<string, double> averages)
-		{
-			if (vectors == null)
-				throw new ArgumentNullException(nameof(vectors));
-			if (averages == null)
-				throw new ArgumentNullException(nameof(averages));
-			if (vectors.Count == 0)
-				return new Dictionary<string, double>();
-
-			IDictionary<string, double> squaredDifference = new Dictionary<string, double>();
-			FeatureVector exampleVector = vectors.First().Value;
-
-			foreach (ElemDescriptor desc in exampleVector.GetDescriptors<ElemDescriptor>())
-				squaredDifference[desc.Name] = 0;
-
-			foreach (FeatureVector vector in vectors.Values)
-				foreach (ElemDescriptor desc in vector.GetDescriptors<ElemDescriptor>())
-					squaredDifference[desc.Name] += Math.Pow(desc.Value - averages[desc.Name], 2);
-
-			double inverseCount = 1d / vectors.Count;
-			foreach (KeyValuePair<string, double> sum in squaredDifference.ToArray())
-				squaredDifference[sum.Key] = Math.Sqrt(sum.Value * inverseCount);
-
-			return squaredDifference;
 		}
 	}
 }
